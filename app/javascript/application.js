ec426e7be7b6a4dd24bb8fc22134aa1ced335255
JorgePadilla <jorgep4dill4@gmail.com>
2022-02-02 02:13:52 -0600

    connect db with view

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,69 +6,18 @@ import * as bootstrap from "bootstrap"
 
 document.addEventListener('DOMContentLoaded', function() {
   var calendarEl = document.getElementById('calendar');
-  // var calendar = new FullCalendar.Calendar(calendarEl, {
-  //   initialView: 'dayGridMonth'
-  // });
-
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
       initialView: 'dayGridMonth',
-      initialDate: '2022-01-07',
+      initialDate: '2022-02-01',
       headerToolbar: {
         left: 'prev,next today',
         center: 'title',
         right: 'dayGridMonth,timeGridWeek,timeGridDay'
       },
-      events: [
-        {
-          title: 'All Day Event',
-          start: '2022-01-01'
-        },
-        {
-          title: 'Long Event',
-          start: '2022-01-07',
-          end: '2022-01-10'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2022-01-09T16:00:00'
-        },
-        {
-          groupId: '999',
-          title: 'Repeating Event',
-          start: '2022-01-16T16:00:00'
-        },
-        {
-          title: 'Conference',
-          start: '2022-01-11',
-          end: '2022-01-13'
-        },
-        {
-          title: 'Meeting',
-          start: '2022-01-12T10:30:00',
-          end: '2022-01-12T12:30:00'
-        },
-        {
-          title: 'Lunch',
-          start: '2022-01-12T12:00:00'
-        },
-        {
-          title: 'Meeting',
-          start: '2022-01-12T14:30:00'
-        },
-        {
-          title: 'Birthday Party',
-          start: '2022-01-13T07:00:00'
-        },
-        {
-          title: 'Click for Google',
-          url: 'http://google.com/',
-          start: '2022-01-28'
-        }
-      ]
+      events: '/mc',
+      eventColor: '#378006'
     });
   
     calendar.render();
-  // calendar.render();
 });
